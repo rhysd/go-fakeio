6d5719de4001dc1fe823ad0f4a838114fea9dc83
rhysd <lin90162@yahoo.co.jp>
2018-10-25 20:51:05 +0900

    add introduction doc for godoc

---
+++ doc.go
@@ -0,0 +1,40 @@
+/*
+Package fakeio is a small library to fake stdout/stderr/stdin mainly for
+unit testing.
+
+To import this library:
+
+    "github.com/rhysd/fakeio"
+
+Following example fakes stdout/stderr/stdin. Set 'from stdin!' as stdin and get
+output from both stdout and stderr as string.
+
+    f := fakeio.Stdout().Stderr().Stdin("from stdin!")
+
+    fromInput, err := bufio.NewReader(os.Stdin).ReadString('!')
+    if err != nil {
+        f.Restore()
+        panic(err)
+    }
+
+    fmt.Println("from stdout!")
+    fmt.Fprintln(os.Stderr, "from stderr!")
+
+    fromOutput, err := f.String()
+    if err != nil {
+        f.Restore()
+        panic(err)
+    }
+
+    // 'defer' is better, but here it's unavailable due to output test
+    f.Restore()
+
+    // Output: from stdin!
+    fmt.Println(fromInput)
+
+    // Output:
+    // from stdout!
+    // from stderr!
+    fmt.Println(fromOutput)
+*/
+package fakeio
